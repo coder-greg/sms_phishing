a50898496e66a92846d671b385c152dcb57410dc
Grzegorz Klimek <grzech66@o2.pl>
2025-08-22 22:23:30 +0200

    Fix PhisingDetecionJob setup

--- jobs/sms-flink-job/src/main/java/pl/example/UserStateManagementJob.java
+++ jobs/sms-flink-job/src/main/java/pl/example/UserStateManagementJob.java
@@ -47,7 +47,6 @@ public class UserStateManagementJob {
     }
 
 
-
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
